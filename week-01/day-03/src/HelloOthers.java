d96bde973971f08e1b689f74c0983afc21942b06
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-10 13:11:55 +0200

    Java exercises 1, 2, 3

---
+++ week-01/day-03/src/HelloOthers.java
@@ -0,0 +1,10 @@
+public class HelloOthers {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Dani!");
+        System.out.println("Hello Gabi!");
+        System.out.println("Hello Jane!");
+
+    }
+}
---
+++ week-01/day-03/src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        String textToPrint = "Hello Seagal!";
+
+        System.out.println(textToPrint);
+
+    }
+}
