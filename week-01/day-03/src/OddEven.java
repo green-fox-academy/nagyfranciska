22df6d0f4d0b8c7dc2eaf5ceac38f7541b6d70fb
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-10 22:02:22 +0200

    commit exercise 18

--- week-01/day-03/src/OddEven.java
+++ week-01/day-03/src/OddEven.java
@@ -1,2 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class OddEven {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please give me a number!");
+        int userNumber = userInput.nextInt();
+
+        if (userNumber % 2 == 0) {
+            System.out.println("Even number.");
+        }   else {
+            System.out.println("Odd number.");
+        }
+
+
+
+
+    }
 }
