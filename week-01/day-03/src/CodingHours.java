330b17eb52fd53023ba99ffc045a9b29b1dda1f3
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-10 13:46:19 +0200

    Java exercises 4, 5, 6

---
+++ week-01/day-03/src/CodingHours.java
@@ -0,0 +1,10 @@
+public class CodingHours {
+
+    public static void main(String[] args) {
+
+        
+        System.out.println(6 * 5 * 17 + " coding hours in a semester.");
+
+        System.out.println((6 * 5) / 52. + " %");
+    }
+}
---
+++ week-01/day-03/src/IntroduceYourself.java
@@ -0,0 +1,10 @@
+public class IntroduceYourself {
+
+    public static void main(String[] args) {
+
+        System.out.println("Nagy Franciska");
+        System.out.println("28");
+        System.out.println("1.63");
+
+    }
+}
