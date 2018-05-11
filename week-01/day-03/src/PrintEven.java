2228ce198d0fd97aabdaa3a5211b0595b305ce08
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-11 11:45:44 +0200

    commit PrintEven

---
+++ week-01/day-03/src/PrintEven.java
@@ -0,0 +1,18 @@
+public class PrintEven {
+
+    public static void main(String[] args) {
+
+        int num = 0;
+
+        for (int i = 0; i < 501; i++) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+
+            }
+            num += 1;
+        }
+
+
+
+    }
+}
