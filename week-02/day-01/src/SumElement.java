b2b5e06abf4a5523207531cad864b9d3a90793cb
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-14 14:41:48 +0200

    commit SumElement

---
+++ week-02/day-01/src/SumElement.java
@@ -0,0 +1,12 @@
+public class SumElement {
+  public static void main(String[] args) {
+
+    int[] r = new int[] {54,23,66,12};
+    int sum = r[0] + r[2];
+    System.out.println(sum);
+
+
+
+  }
+
+}
