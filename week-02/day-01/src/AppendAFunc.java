e3401a070b0123c95671fc0f94f200d78a0b2f64
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-14 13:28:23 +0200

    commit AppendA

---
+++ week-02/day-01/src/AppendAFunc.java
@@ -0,0 +1,11 @@
+public class AppendAFunc {
+  public static void main(String[] args) {
+
+    String am = "kuty";
+    System.out.println(appendChar(am));
+  }
+  public static String appendChar(String appendA) {
+    return appendA + "a";
+  }
+
+}
