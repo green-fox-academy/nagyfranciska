0357733afdb3f62220a9d04334d2710115d46b6c
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-14 12:59:46 +0200

    commit Doubling

---
+++ week-02/day-01/src/Doubling.java
@@ -0,0 +1,14 @@
+public class Doubling {
+
+  public static void main(String[] args) {
+
+    int baseNum = 123;
+    System.out.println(doubling(baseNum));
+
+  }
+  public static int doubling(int doubleNum) {
+    doubleNum *= 2;
+    return doubleNum;
+
+  }
+}
