2b5bb8f432630eb26433e664a8d92716a0ff583d
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-16 14:10:01 +0200

    commit WriteSingleLine and created new-file.txt

---
+++ week-02/day-03/src/WriteSingleLine.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteSingleLine {
+  public static void main(String[] args) {
+
+    // Open a file called "my-file.txt"
+    // Write your name in it as a single line
+    // If the program is unable to write the file,
+    // then it should print an error message like:
+    // "Unable to write file: my-file.txt"
+
+    List<String> content = new ArrayList<>();
+    content.add("Nagy Franciska");
+    Path path = Paths.get("assets/new-file.txt");
+    try {
+      Files.write(path,content);
+    } catch (IOException e) {
+      System.out.println("Unable to write file: my-file.txt");
+    }
+
+  }
+}
