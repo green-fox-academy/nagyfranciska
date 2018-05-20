3046e8b57dc1eaab17ba8e4dffd876069171c45f
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 22:14:58 +0200

    refact(MatchMaking): add instruction

--- week-02/day-02/src/Matchmaking.java
+++ week-02/day-02/src/Matchmaking.java
@@ -5,20 +5,21 @@ import java.util.Arrays;
 public class Matchmaking {
   public static void main(String[] args) {
 
+    // Write a method that joins the two lists by matching
+    // one girl with one boy into a new list.
+    // Expected output: "Eve", "Joe", "Ashley", "Fred"...
+
     ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Bözsi","Kat","Jane"));
     ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Béla","Todd","Neef","Jeff"));
-
-    System.out.println(makingMatches(girls, boys));
+    System.out.println(MakingMatches(girls, boys));
 
   }
-  public static ArrayList makingMatches(ArrayList list1, ArrayList list2) {
 
+  public static ArrayList MakingMatches(ArrayList list1, ArrayList list2) {
     ArrayList<String> newList = new ArrayList<>();
-    for (int i = 0; i < list1.size() ; i++) {
+    for (int i = 0; i < list1.size(); i++) {
       newList.add(list1.get(i).toString());
       newList.add(list2.get(i).toString());
-    }
-    return newList;
+    } return newList;
   }
-
 }
