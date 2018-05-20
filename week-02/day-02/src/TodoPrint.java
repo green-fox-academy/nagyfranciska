a260128cd53e2f4789fae071e6f6f497deada138
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 21:51:21 +0200

    refact(ToDoPrint): add instruction

--- week-02/day-02/src/TodoPrint.java
+++ week-02/day-02/src/TodoPrint.java
@@ -5,6 +5,17 @@ public class TodoPrint {
 
     String todoText = " - Buy milk\n";
 
+    // Add "My todo:" to the beginning of the todoText
+    // Add " - Download games" to the end of the todoText
+    // Add " - Diablo" to the end of the todoText but with indention
+
+    // Expected outpt:
+
+    // My todo:
+    //  - Buy milk
+    //  - Download games
+//      - Diablo
+
     todoText = "My todo:\n" + todoText + " - Download games\n" + "    - Diablo";
     System.out.println(todoText);
 
