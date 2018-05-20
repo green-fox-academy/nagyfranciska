838f4a390ed752538ff2a5d4bdede9ab44797dd1
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 22:22:59 +0200

    refact(AppendLetter): add instructions

--- week-02/day-02/src/AppendLetter.java
+++ week-02/day-02/src/AppendLetter.java
@@ -4,17 +4,20 @@ import java.util.List;
 public class AppendLetter {
   public static void main(String[] args) {
 
+    // Create a method called "appendA()" that adds "a" to every string in the "far" list.
+    // The parameter should be a list.
+
     List<String> far = Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk");
     System.out.println(appendA(far));
   }
 
-  public static List appendA(List addChar) {
+  public static List appendA(List list) {
     String animal = "";
-    for (int i = 0; i < addChar.size(); i++) {
-      animal = addChar.get(i).toString();
-      animal += "a";
-      addChar.set(i,(Object)animal);
-    }
-    return addChar;
+    for (int i = 0; i < list.size(); i++) {
+    animal = list.get(i).toString();
+    animal += "a";
+    list.set(i, animal);
+   }
+    return list;
   }
 }
