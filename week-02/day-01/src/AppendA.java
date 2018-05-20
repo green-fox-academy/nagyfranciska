8b263785fe56957288e8dbd35aac3a04a94820eb
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 19:45:31 +0200

    refact(AppendA): add instruction

--- week-02/day-01/src/AppendA.java
+++ week-02/day-01/src/AppendA.java
@@ -3,15 +3,16 @@ import java.util.Arrays;
 public class AppendA {
   public static void main(String[] args) {
 
+    // - Create an array variable named `animals`
+    //   with the following content: `["kuty", "macsk", "cic"]`
+    // - Add all elements an `"a"` at the end
+
     String[] animals = new String[] {"kuty", "macsk", "cic"};
 
     for (int i = 0; i < animals.length; i++) {
-      animals[i] = animals[i] + "a";
-
+      animals[i] += "a";
     }
-
     System.out.println(Arrays.toString(animals));
 
   }
-
 }
