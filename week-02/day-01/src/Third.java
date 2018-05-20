c8b64e038702631b7d94a200fb1216e6df280b7d
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 18:17:07 +0200

    refact(Third): add instructions

--- week-02/day-01/src/Third.java
+++ week-02/day-01/src/Third.java
@@ -1,9 +1,13 @@
 public class Third {
   public static void main(String[] args) {
 
-    int[] q = new int[] {4,5,6,7};
+    // - Create an array variable named `q`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `q`
+    //
+
+    int[] q = {4,5,6,7};
     System.out.println(q[2]);
 
   }
-
 }
