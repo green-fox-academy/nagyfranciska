46c55bf7a6eaf16d6d71d583c166c06584c4a295
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 12:34:55 +0200

    refact(DefineBasicInfo): correct indentation

--- week-01/day-03/src/DefineBasicInfo.java
+++ week-01/day-03/src/DefineBasicInfo.java
@@ -1,27 +1,25 @@
 public class DefineBasicInfo {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    // Define several things as a variable then print their values
+    // Your name as a string
+    // Your age as an integer
+    // Your height in meters as a double
+    // Whether you are married or not as a boolean
 
-        String myName = "Nagy Franciska";
-        int myAge = 28;
-        double myHeight = 1.63;
-        boolean married = false;
+    String myName = "Nagy Franciska";
+    int myAge = 28;
+    double myHeight = 1.63;
+    boolean married = false;
 
-        System.out.println(myName);
+    System.out.println(myName);
+    System.out.println(myAge);
+    System.out.println(myHeight);
+    if (married) {
+      System.out.println("I am happily married.");
+      } else {
+      System.out.println("I am not married.");
+      }
 
-        System.out.println(myAge);
-
-        System.out.println(myHeight);
-
-        if (married) {
-            System.out.println("I am happily married.");
-        }   else {
-            System.out.println("I am not married.");
-        }
-
-
-
-
-
-    }
+  }
 }
