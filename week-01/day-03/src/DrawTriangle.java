b1834821a830659fdd1964ad317002abb4a31fe7
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 15:16:40 +0200

    refact(DrawTriangle): add instruction and correct indentation

--- week-01/day-03/src/DrawTriangle.java
+++ week-01/day-03/src/DrawTriangle.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
 
 public class DrawTriangle {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    // Write a program that reads a number from
+    // the standard input, then draws a triangle like this:
+    //
+    // *
+    // **
+    // ***
+    // ****
+    //
+    // The triangle should have as many lines as the number was
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Give me a number:");
-        int userInput = scan.nextInt();
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int userInput = scan.nextInt();
+    String pattern = "*";
 
-        String pattern = "*";
-
-        for (int raw = 0; raw < userInput; raw++) {
-
-            System.out.println(pattern);
-            pattern += "*";
-        }
-
-
-
-
-
-
-
-
-
+    for (int i = 0; i < userInput; i++) {
+      System.out.println(pattern);
+      pattern += "*";
     }
+
+  }
 }
