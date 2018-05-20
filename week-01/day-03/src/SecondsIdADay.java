0374c80ba6c66dd43d2c22414d791d7805f4975b
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 13:09:47 +0200

    refact(SecondsInDay): add instructions and correct indentation

--- week-01/day-03/src/SecondsIdADay.java
+++ week-01/day-03/src/SecondsIdADay.java
@@ -1,16 +1,16 @@
 public class SecondsIdADay {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    // Write a program that prints the remaining seconds (as an integer) from a
+    // day if the current time is represented by the variables
 
-        int currentHours = 14;
-        int currentMinutes = 34;
-        int currentSeconds = 42;
+    int currentHours = 14;
+    int currentMinutes = 34;
+    int currentSeconds = 42;
 
-        int wholeDayInSeconds = 24 * 60 * 60;
-        int currentTime = currentHours * 60 * 60 + currentMinutes * 60 + currentSeconds;
+    int wholeDayInSeconds = 24 * 60 * 60;
+    int currentTime = currentHours * 60 * 60 + currentMinutes * 60 + currentSeconds;
+    System.out.println ("Remaining seconds : " + (wholeDayInSeconds - currentTime));
 
-
-        System.out.println ("Remaining seconds : " + (wholeDayInSeconds - currentTime));
-
-    }
+  }
 }
