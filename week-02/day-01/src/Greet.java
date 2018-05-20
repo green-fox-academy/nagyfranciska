b1dfc979ca4c6d5150bfe168e172ba3eb03fcc48
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 16:34:31 +0200

    refact(Greet): add instructions

--- week-02/day-01/src/Greet.java
+++ week-02/day-01/src/Greet.java
@@ -1,10 +1,18 @@
 public class Greet {
   public static void main(String[] args) {
 
+    // - Create a string variable named `al` and assign the value `Greenfox` to it
+    // - Create a function called `greet` that greets it's input parameter
+    // - Greeting is printing e.g. `Greetings dear, Greenfox`
+    // - Greet `al`
+
     String a1 = "Greenfox";
     System.out.println(greet(a1));
+
   }
+
     public static String greet(String userGreeting) {
-      return "Greetings dear, " + userGreeting;
-    }
+    return "Greetings dear, " + userGreeting;
+
+  }
 }
