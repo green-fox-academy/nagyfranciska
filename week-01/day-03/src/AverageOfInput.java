78d90cc0938b5208040abce8c10b33ba26c1be14
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 14:06:52 +0200

    refact(AverageofInput): correct scanner and indentation

--- week-01/day-03/src/AverageOfInput.java
+++ week-01/day-03/src/AverageOfInput.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
 public class AverageOfInput {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    // Write a program that asks for 5 integers in a row,
+    // then it should print the sum and the average of these numbers like:
+    //
+    // Sum: 22, Average: 4.4
 
-        float a = 5;
-        int userInput = 0;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter 5 numbers: ");
+    int userInput = 0;
 
-        for (int i = 0; i < a; i++) {
-            Scanner askForNumber = new Scanner(System.in);
-            System.out.println("Please give me a number: ");
-            userInput += askForNumber.nextInt();
-        }
-
-        System.out.println("Sum: " + userInput);
-        System.out.println("Average: " + userInput / a);
-
-
+    for (int i = 0; i < 5; i++) {
+      userInput += scanner.nextInt();
     }
+    System.out.println("Sum: " + userInput);
+    System.out.println("Average: " + userInput/5.);
+
+  }
 }
