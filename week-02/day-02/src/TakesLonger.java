c8b8f8b0bcdd5e67a593a548bbebfb728f36bdc0
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 21:49:24 +0200

    refact(TakesLonger): add instructions

--- week-02/day-02/src/TakesLonger.java
+++ week-02/day-02/src/TakesLonger.java
@@ -3,7 +3,11 @@ public class TakesLonger {
 
     String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
 
-    quote = quote.substring(0,quote.indexOf("you")) + "always takes longer than " + quote.substring(quote.indexOf("you"));
+    // When saving this quote a disk error has occurred. Please fix it.
+    // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+    // Using pieces of the quote variable (instead of just redefining the string)
+
+    quote = quote.substring(0, quote.indexOf("you ")) + "always takes longer than " + quote.substring(quote.indexOf("you"));
     System.out.println(quote);
   }
 }
