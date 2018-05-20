2f86bf282156971a5d0df30c8440d69bb039539f
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 19:33:38 +0200

    feat(DiagonalMatrix): correct loop

--- week-02/day-01/src/DiagonalMatrix.java
+++ week-02/day-01/src/DiagonalMatrix.java
@@ -1,21 +1,31 @@
 public class DiagonalMatrix {
   public static void main(String[] args) {
 
+    // - Create (dynamically) a two dimensional array
+    //   with the following matrix. Use a loop!
+    //
+    //   1 0 0 0
+    //   0 1 0 0
+    //   0 0 1 0
+    //   0 0 0 1
+    //
+    // - Print this two dimensional array to the output
 
+    int[][] matrix = new int[4][4];
 
-
+    for (int row = 0; row < matrix.length; row++) {
+      for (int column = 0; column < matrix.length; column++) {
+        if (row == column) {
+          matrix[row][column] = 1;
+          System.out.print(matrix[row][column] + " ");
+        } else {
+          matrix[row][column] = 0;
+          System.out.print(matrix[row][column] + " ");
+        }
+      }
+      System.out.println(" ");
+    }
 
   }
-
-  // - Create (dynamically) a two dimensional array
-  //   with the following matrix. Use a loop!
-  //
-  //   1 0 0 0
-  //   0 1 0 0
-  //   0 0 1 0
-  //   0 0 0 1
-  //
-// - Print this two dimensional array to the output
-
-
 }
+
