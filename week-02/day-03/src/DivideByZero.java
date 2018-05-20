02ae9e42a07de958d943f8f6272bf2b3868b1f40
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 22:42:30 +0200

    refact(DevideByZero): correct method

--- week-02/day-03/src/DivideByZero.java
+++ week-02/day-03/src/DivideByZero.java
@@ -8,18 +8,18 @@ public class DivideByZero {
     // It should print "fail" if the parameter is 0
 
     Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a number: ");
     int userInput = scanner.nextInt();
+    DivideFunction(userInput);
 
+  }
+
+  public static void DivideFunction(int number) {
     try {
-      int result = 10/divideFunction(userInput);
+      int result = 10/number;
       System.out.println(result);
     } catch(Exception e) {
       System.out.println("fail");
     }
-
-  }
-
-  public static int divideFunction(int randomNumber) {
-    return randomNumber;
   }
 }
