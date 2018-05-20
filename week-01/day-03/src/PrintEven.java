0fc096ca9e65672999d71e59c2822147776d928e
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 14:57:13 +0200

    refact(PrintEven): correct loop and indentation

--- week-01/day-03/src/PrintEven.java
+++ week-01/day-03/src/PrintEven.java
@@ -1,18 +1,13 @@
 public class PrintEven {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    // Create a program that prints all the even numbers between 0 and 500
 
-        int num = 0;
-
-        for (int i = 0; i < 501; i++) {
-            if (num % 2 == 0) {
-                System.out.println(num);
-
-            }
-            num += 1;
-        }
-
-
-
+    for (int i = 0; i < 501; i++) {
+      if (i % 2 == 0) {
+        System.out.println(i);
+      }
     }
+
+  }
 }
