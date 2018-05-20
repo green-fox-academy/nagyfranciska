13ea69362c6f5250ba77ec06a80eb4bbff3e970a
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 12:31:20 +0200

    refact(Bmi): correct indentation

--- week-01/day-03/src/Bmi.java
+++ week-01/day-03/src/Bmi.java
@@ -1,10 +1,9 @@
 public class Bmi {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    double massInKg = 81.2;
+    double heightInM = 1.78;
+    System.out.println("BMI value is: " + massInKg / Math.pow(heightInM, 2));
 
-      double massInKg = 81.2;
-      double heightInM = 1.78;
-
-      System.out.println("BMI value is: " + massInKg / Math.pow(heightInM,2));
-    }
+  }
 }
