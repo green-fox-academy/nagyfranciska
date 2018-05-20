e689e1dd36d0e39c9438ce3bf375975dbfd60de0
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 15:12:20 +0200

    refact(FizzBuzz): correct loop and indentation

--- week-01/day-03/src/FizzBuzz.java
+++ week-01/day-03/src/FizzBuzz.java
@@ -1,27 +1,25 @@
 public class FizzBuzz {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    // Write a program that prints the numbers from 1 to 100.
+    // But for multiples of three print “Fizz” instead of the number
+    // and for the multiples of five print “Buzz”.
+    // For numbers which are multiples of both three and five print “FizzBuzz”.
 
-        int number = 1;
-        String fizz = "Fizz";
-        String buzz = "Buzz";
+    String fizz = "Fizz";
+    String buzz = "Buzz";
 
-        for (int i = 0; i < 100; i++) {
-            if (number % 3 == 0 && number % 5 == 0) {
-                System.out.println(fizz + buzz);
-            }   else if (number % 3 == 0) {
-                System.out.println(fizz);
-            }   else if (number % 5 == 0) {
-                System.out.println(buzz);
-            }   else {
-                System.out.println(number);
-            }
-            number += 1;
-        }
-
-
-
-
+    for (int i = 1; i < 101; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println(fizz + buzz);
+      } else if (i % 3 == 0) {
+        System.out.println(fizz);
+      } else if (i % 5 == 0) {
+        System.out.println(buzz);
+      } else {
+        System.out.println(i);
+      }
     }
 
+  }
 }
