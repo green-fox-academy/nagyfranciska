5ef7e60a528cc1a2f299642257b238fddb277da0
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 18:23:43 +0200

    refact(SumElement): correct array and index

--- week-02/day-01/src/SumElement.java
+++ week-02/day-01/src/SumElement.java
@@ -1,12 +1,13 @@
 public class SumElement {
   public static void main(String[] args) {
 
-    int[] r = new int[] {54,23,66,12};
-    int sum = r[0] + r[2];
+    // - Create an array variable named `r`
+    //   with the following content: `[54, 23, 66, 12]`
+    // - Print the sum of the second and the third element
+
+    int[] r = {54, 23, 66, 12};
+    int sum = r[1] + r[2];
     System.out.println(sum);
 
-
-
   }
-
 }
