9b732a4490f55d176e353912967fbd3911079c2d
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 22:07:33 +0200

    refact(SolarSystem): correct index in ArrayList

--- week-02/day-02/src/SolarSystem.java
+++ week-02/day-02/src/SolarSystem.java
@@ -1,18 +1,22 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SolarSystem {
   public static void main(String[] args) {
 
-    ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"));
+    // Saturn is missing from the planetList
+    // Insert it into the correct position
+    // Create a method called putSaturn() which has
+    // list parameter and returns the correct list.
+
+    ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"));
     System.out.println(putSaturn(planetList));
 
   }
-  public static ArrayList putSaturn(ArrayList putSaturn) {
-    putSaturn.add("Saturn");
-    return putSaturn;
 
+  public static ArrayList putSaturn(ArrayList planet) {
+    planet.add(5, "Saturn");
+    return planet;
 
   }
 }
