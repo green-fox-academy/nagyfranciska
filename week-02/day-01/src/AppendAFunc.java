f03cfc4849c6556a12c3faaa18cc85da5bcf4bc4
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 16:39:47 +0200

    refact(AppendAFunc): add instruction and correct function

--- week-02/day-01/src/AppendAFunc.java
+++ week-02/day-01/src/AppendAFunc.java
@@ -1,11 +1,16 @@
 public class AppendAFunc {
   public static void main(String[] args) {
 
-    String am = "kuty";
-    System.out.println(appendChar(am));
-  }
-  public static String appendChar(String appendA) {
-    return appendA + "a";
-  }
+    // - Create a string variable named `am` and assign the value `kuty` to it
+    // - Write a function called `appendA` that gets a string as an input,
+    //   appends an 'a' character to its end and returns with a string
+    // - Print the result of `appendAFunc(am)`
 
+    String am = "kuty";
+    System.out.println(appendAFunc(am));
+
+  }
+  public static String appendAFunc(String input) {
+    return input + "a";
+  }
 }
