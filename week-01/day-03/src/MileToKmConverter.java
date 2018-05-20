1468527059bfd7365a03b1cc147bf7105f03d989
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 13:56:04 +0200

    refact(MiletoKmConverter): add instructions and correnst indentation

--- week-01/day-03/src/MileToKmConverter.java
+++ week-01/day-03/src/MileToKmConverter.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class MileToKmConverter {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    // Write a program that asks for an integer that is a distance in kilometers,
+    // then it converts that value to miles and prints it
 
-        Scanner distanceInKm = new Scanner(System.in);
-        System.out.println("Please give me the distance in km!");
-        int userInput = distanceInKm.nextInt();
+    Scanner distanceInKm = new Scanner(System.in);
+    System.out.println("Enter the distance in km: ");
+    double userInput = distanceInKm.nextInt();
 
-        double convNumber = userInput * 0.621371;
+    double distanceInMiles = userInput * 0.621371;
+    System.out.println(distanceInMiles);
 
-        System.out.println(convNumber);
-
-
-    }
+  }
 }
