2cf5749f966e580d66681f702b7fbda2c82f9d37
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 14:32:58 +0200

    refact(PartyIndicator): re-arrange conditions

--- week-01/day-03/src/PartyIndicator.java
+++ week-01/day-03/src/PartyIndicator.java
@@ -1,23 +1,37 @@
 import java.util.Scanner;
 
 public class PartyIndicator {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    // Write a program that asks for two numbers
+    // Thw first number represents the number of girls that comes to a party, the
+    // second the boys
+    // It should print: The party is excellent!
+    // If the the number of girls and boys are equal and there are more people coming than 20
+    //
+    // It should print: Quite cool party!
+    // It there are more than 20 people coming but the girl - boy ratio is not 1-1
+    //
+    // It should print: Average party...
+    // If there are less people coming than 20
+    //
+    // It should print: Sausage party
+    // If no girls are coming, regardless the count of the people
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please give me two numbers, the first represents the girls, the second the boys: ");
-        int number1 = scan.nextInt();
-        int number2 = scan.nextInt();
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter two numbers, one for the girls, one for the boys: ");
+    int girls = scan.nextInt();
+    int boys = scan.nextInt();
 
-        if (number1 == number2 && (number1 + number2) > 20) {
-            System.out.println("The party is excellent!");
-        } else if (number1 != number2 && (number1 + number2) > 20) {
-            System.out.println("Quite cool party!");
-        } else if ((number1 + number2) < 20) {
-            System.out.println("Average party...");
-        } else {
-            System.out.println("Sausage party");
-        }
-
+    if (girls == boys && (girls + boys) > 20) {
+      System.out.println("The party is excellent!");
+    } else  if (girls == 0){
+      System.out.println("Sausage party");
+    } else if (girls != boys && (girls + boys) > 20) {
+        System.out.println("Quite cool party!");
+    } else if (girls + boys < 20) {
+        System.out.println("Average party...");
     }
+
+  }
 }
