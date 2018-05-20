eec46eddd859f230eb3b939a52d08b4bf7138dac
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 19:36:08 +0200

    refact(DoubleItems): add instruction

--- week-02/day-01/src/DoubleItems.java
+++ week-02/day-01/src/DoubleItems.java
@@ -3,13 +3,16 @@ import java.util.Arrays;
 public class DoubleItems {
   public static void main(String[] args) {
 
-    int[] numList = new int[] {3,4,5,6,7};
+    // - Create an array variable named `numList`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Double all the values in the array
 
-    for (int i = 0; i <numList.length ; i++) {
+    int[] numList = new int[] {3, 4, 5, 6, 7};
+
+    for (int i = 0; i < numList.length ; i++) {
       numList[i] *= 2;
     }
     System.out.println(Arrays.toString(numList));
-
 
   }
 }
