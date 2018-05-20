1ebb7d7bf97358d4a9bac855c29cac9d53c940a6
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 15:01:54 +0200

    refact(MultiplicationTable): simpify and correct indentation

--- week-01/day-03/src/MultiplicationTable.java
+++ week-01/day-03/src/MultiplicationTable.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
 
 public class MultiplicationTable {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    // Create a program that asks for a number and
+    // prints the multiplication table with that number
+    //
+    // Example:
+    // The number 15 should print:
+    //
+    // 1 * 15 = 15
+    // 2 * 15 = 30
+    // 3 * 15 = 45
+    // 4 * 15 = 60
+    // 5 * 15 = 75
+    // 6 * 15 = 90
+    // 7 * 15 = 105
+    // 8 * 15 = 120
+    // 9 * 15 = 135
+    // 10 * 15 = 150
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please give me a number!");
-        int userInput = scan.nextInt();
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int userInput = scan.nextInt();
 
-        int multip = 10;
-
-        for (int i = 1; i < (multip + 1); i++) {
-            System.out.println(i + " * " + userInput + " = " + i * userInput);
-
-        }
-
-
-        
-
-
-
-
-
-
-
-
+    for (int i = 1; i < 11; i++) {
+      System.out.println(i + " * " + userInput + " = " + i * userInput);
     }
+
+  }
 }
