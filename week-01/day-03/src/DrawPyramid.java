3bd1bbb3bd4316e76678822b37b1431c6cc0e9ef
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 15:27:59 +0200

    refact(DrawPyramid): add instruction and simplify loop

--- week-01/day-03/src/DrawPyramid.java
+++ week-01/day-03/src/DrawPyramid.java
@@ -1,28 +1,34 @@
 import java.util.Scanner;
 
 public class DrawPyramid {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    // Write a program that reads a number from the standard input, then draws a
+    // pyramid like this:
+    //
+    //
+    //    *
+    //   ***
+    //  *****
+    // *******
+    //
+    // The pyramid should have as many lines as the number was
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Give me a number:");
-        int userInput = scan.nextInt();
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number:");
+    int userInput = scan.nextInt();
+    String pattern = "*";
+    String emptyP = " ";
+    int num = userInput;
 
-        String pattern = "*";
-        String emptyP = " ";
-        int num = userInput;
-
-        for (int r = 0; r < userInput; r++) {
-
-            for (int c = 0; c < num - 1; c++) {
-                System.out.print(emptyP);
-            }
-            System.out.println(pattern);
-            pattern += "**";
-            num -= 1;
-
-        }
-
-
+    for (int i = 0; i < userInput; i++) {
+      for (int j = num - 1; j > 0; j--) {
+        System.out.print(emptyP);
+      }
+      System.out.println(pattern);
+      pattern += "**";
+      num -= 1;
     }
+
+  }
 }
