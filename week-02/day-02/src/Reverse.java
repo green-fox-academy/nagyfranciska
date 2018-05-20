820eee024fcf48d343cf0ed8ee13452c3edcae52
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 21:55:40 +0200

    refact(Reverse): add instruction and tidy up loop

--- week-02/day-02/src/Reverse.java
+++ week-02/day-02/src/Reverse.java
@@ -1,16 +1,20 @@
 public class Reverse {
   public static void main(String[] args) {
 
-    String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+    // Create a method that can reverse a String, which is passed as the parameter.
+    // Use it on this reversed string to check it!
+    // Try to solve this using charAt() first, and optionally anything else after.
 
-    System.out.println(reverse(reversed));
+    String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+    System.out.println(Reverse(reversed));
+
   }
-  public static String reverse (String textToBack){
+
+  public static String Reverse (String textToBack){
     String text = "";
-    for(int i=textToBack.length() -1;i>=0;i--) {
-      text = text + textToBack.charAt(i);
+    for (int i = textToBack.length() - 1; i >= 0; i--) {
+      text += textToBack.charAt(i);
     }
     return text;
-
   }
 }
