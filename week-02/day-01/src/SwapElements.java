39de6fcc96f598d85b7f01a0882d317829d2a976
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 19:49:35 +0200

    refact(SwapElements): add instruction and correct typo

--- week-02/day-01/src/SwapElements.java
+++ week-02/day-01/src/SwapElements.java
@@ -3,18 +3,15 @@ import java.util.Arrays;
 public class SwapElements {
   public static void main(String[] args) {
 
+    // - Create an array variable named `abc`
+    //   with the following content: `["first", "second", "third"]`
+    // - Swap the first and the third element of `abc`
+
     String[] abc = new String[] {"first","second","third"};
-
-    String temperal = abc[0];
+    String temporal = abc[0];
     abc[0] = abc[2];
-    abc[2] = temperal;
-    System.out.println(Arrays.toString(abc));
-
-    // more handy way to swap first and last elements:
-
-    // String temperal = abc[0];
-    // abc[0] = abc[abc.length -1];
-    // abc[abc.length -1] = temperal;
+    abc[2] = temporal;
     // System.out.println(Arrays.toString(abc));
+
   }
 }
