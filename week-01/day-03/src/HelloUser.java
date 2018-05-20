fcee592d43dbd2301907ef005a182a9c0223f96b
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 13:52:16 +0200

    refact(HelloUser): correct indentation

--- week-01/day-03/src/HelloUser.java
+++ week-01/day-03/src/HelloUser.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
 public class HelloUser {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    // Modify this program to greet user instead of the World!
+    // The program should ask for the name of the user
 
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("Please give me your name!");
-        String userName = userInput.nextLine();
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    String userName = userInput.nextLine();
 
-        System.out.println("Hello, " + userName + "!");
+    System.out.println("Hello, " + userName + "!");
 
-    }
+  }
 }
