02fbca821391e699486dcb24e0e45569405a9ece
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 16:31:46 +0200

    refact(Doubling): add instructions

--- week-02/day-01/src/Doubling.java
+++ week-02/day-01/src/Doubling.java
@@ -1,11 +1,15 @@
 public class Doubling {
-
   public static void main(String[] args) {
+
+    // - Create an integer variable named `baseNum` and assign the value `123` to it
+    // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+    // - Print the result of `doubling(baseNum)`
 
     int baseNum = 123;
     System.out.println(doubling(baseNum));
 
   }
+
   public static int doubling(int doubleNum) {
     doubleNum *= 2;
     return doubleNum;
