16345154a3993e4bf9abf762dac1fb996113dd3d
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 22:57:28 +0200

    refact(WriteMultipleLines): add instruction

--- week-02/day-03/src/WriteMultipleLines.java
+++ week-02/day-03/src/WriteMultipleLines.java
@@ -9,9 +9,18 @@ import java.util.Scanner;
 public class WriteMultipleLines {
   public static void main(String[] args) {
 
+    // Create a function that takes 3 parameters: a path, a word and a number,
+    // than it should write to a file.
+    // The path parameter should be a string, that describes the location of the file.
+    // The word parameter should be a string, that will be written to the file as lines
+    // The number parameter should describe how many lines the file should have.
+    // So if the word is "apple" and the number is 5, than it should write 5 lines
+    // to the file and each line should be "apple"
+    // The function should not raise any error if it could not write the file.
+
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("Give me a path and file name: ");
+    System.out.println("Enter path and file name: ");
     String location = scan.nextLine();
 
     System.out.println("Give me a word: ");
@@ -23,7 +32,7 @@ public class WriteMultipleLines {
     System.out.println(MultipleLines(location,word,number));
   }
 
-  public static String MultipleLines (String location,String word, Integer number) {
+  public static String MultipleLines (String location, String word, int number) {
     List<String> content = new ArrayList<>();
     for (int i = 0; i < number; i++) {
       content.add(word);
