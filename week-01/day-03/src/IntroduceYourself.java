77d7e999f1dff3cdfd10e749956a81aa48207b87
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 12:22:33 +0200

    refact(IntroduceYouself): correct indentation

--- week-01/day-03/src/IntroduceYourself.java
+++ week-01/day-03/src/IntroduceYourself.java
@@ -1,10 +1,9 @@
 public class IntroduceYourself {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    System.out.println("Nagy Franciska");
+    System.out.println("28");
+    System.out.println("1.63");
 
-        System.out.println("Nagy Franciska");
-        System.out.println("28");
-        System.out.println("1.63");
-
-    }
+  }
 }
