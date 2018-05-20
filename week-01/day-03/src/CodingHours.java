523c82ea2c4e0128177dd39d090d25592f646d51
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 12:20:43 +0200

    refact(CodingHours): correct indentation

--- week-01/day-03/src/CodingHours.java
+++ week-01/day-03/src/CodingHours.java
@@ -1,10 +1,17 @@
 public class CodingHours {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    // An average Green Fox attendee codes 6 hours daily
+    // The semester is 17 weeks long
+    //
+    // Print how many hours is spent with coding in a semester by an attendee,
+    // if the attendee only codes on workdays.
+    //
+    // Print the percentage of the coding hours in the semester if the average
+    // work hours weekly is 52
 
+    System.out.println(6 * 5 * 17 + " hours coding in a semester.");
+    System.out.println((6 * 5) / 52. * 100  + " %");
 
-        System.out.println(6 * 5 * 17 + " coding hours in a semester.");
-
-        System.out.println((6 * 5) / 52. * 100 + " %");
-    }
+  }
 }
