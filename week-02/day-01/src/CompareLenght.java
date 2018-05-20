57dc16dbaf00b2022b953c9d3cdf1c34bd85b4c0
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-20 18:21:31 +0200

    refact(CompareLenght): simplify arrays

--- week-02/day-01/src/CompareLenght.java
+++ week-02/day-01/src/CompareLenght.java
@@ -1,15 +1,20 @@
 public class CompareLenght {
   public static void main(String[] args) {
 
-    int[] p1 = new int[]{1, 2, 3};
-    int[] p2 = new int[]{4, 5};
+    // - Create an array variable named `p1`
+    //   with the following content: `[1, 2, 3]`
+    // - Create an array variable named `p2`
+    //   with the following content: `[4, 5]`
+    // - Print if `p2` has more elements than `p1`
 
+    int[] p1 = {1, 2, 3};
+    int[] p2 = {4, 5};
 
     if (p1.length > p2.length) {
-      System.out.println("p1 has more element than p2");
+      System.out.println("p1 has more elements than p2");
     } else {
-      System.out.println("p2 has more element than p1");
+      System.out.println("p2 has more elements than p1");
     }
-  }
 
+  }
 }
