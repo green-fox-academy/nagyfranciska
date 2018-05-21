a04eec0333ceffbb26abe5c8e6a0d58a03b5efd5
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-21 09:21:11 +0200

    feat(Logs): add Function for Post/Get Ratio

--- week-02/day-03/src/Logs.java
+++ week-02/day-03/src/Logs.java
@@ -15,23 +15,44 @@ public class Logs {
 
     String data = "assets/log.txt";
     System.out.println(IPAddress(data));
+    System.out.println(GetPostRatio(data));
 
   }
-  public static String IPAddress(String data) {
+
+  public static List<String> IPAddress(String data) {
     Path path = Paths.get(data);
-    List<String> dataLine = new ArrayList<>();
     List<String> IPs = new ArrayList<>();
     try {
-      dataLine = Files.readAllLines(path);
-      System.out.println(dataLine.size());
-
-      for (int i = 0; i < dataLine.size(); i++) {
-      IPs.add(dataLine.get(i).split(" ")[8]);
+      List<String> log = Files.readAllLines(path);
+      for (int i = 0; i < log.size(); i++) {
+      IPs.add(log.get(i).split(" ")[8]);
       }
     } catch (IOException e) {
       System.out.println("File not found.");
     }
-    return IPs.toString();
+    return IPs;
   }
 
+  public static double GetPostRatio(String data) {
+    Path path = Paths.get(data);
+    List<String> getAndPost = new ArrayList<>();
+    int get = 0;
+    int post = 0;
+    try {
+      List<String> log = Files.readAllLines(path);
+      for (int i = 0; i < log.size(); i++) {
+        getAndPost.add(log.get(i).split(" ")[11]);
+      }
+      for (String pOrG : getAndPost) {
+        if (pOrG.equals("POST")) {
+          post += 1;
+        } else {
+          get += 1;
+        }
+      }
+    } catch (IOException e) {
+      System.out.println("File not found.");
+    }
+    return (double) get/post;
+  }
 }
