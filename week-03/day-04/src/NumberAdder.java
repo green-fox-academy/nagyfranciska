be80d08795e057bc08e1f4839c347123ca37fdf1
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-24 11:15:22 +0200

    commit NumberAdder

--- week-03/day-04/src/NumberAdder.java
+++ week-03/day-04/src/NumberAdder.java
@@ -4,6 +4,15 @@ public class NumberAdder {
     // Write a recursive function that takes
     // one parameter: n and adds numbers from 1 to n.
 
-    
+    int number = 12;
+    System.out.println(adder(number));
+  }
+
+  private static int adder(int n) {
+    if (n == 1) {
+      return n;
+    } else {
+      return n + adder(n-1);
+    }
   }
 }
