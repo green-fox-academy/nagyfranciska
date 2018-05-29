ba0059429a98abb94f233b8f845c61d4071341eb
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-29 12:42:43 +0200

    feat(GreenFoxOrganisation): create field, methods and constructors in Mentor

--- week-04/day-02/src/GreenFoxOrganisation/Mentor.java
+++ week-04/day-02/src/GreenFoxOrganisation/Mentor.java
@@ -1,4 +1,29 @@
 package GreenFoxOrganisation;
 
-public class Mentor {
+public class Mentor extends Person {
+
+  String level;
+
+  public Mentor(String name, int age, String gender) {
+    super(name, age, gender);
+    level = "intermediate";
+  }
+
+  public Mentor(String name, int age, String gender, String level) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.level = level;
+  }
+
+  @Override
+  public void getGoal() {
+    System.out.println("Educate brilliant junior software developers.");
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old " +
+                        gender + level + " mentor.");
+  }
 }
