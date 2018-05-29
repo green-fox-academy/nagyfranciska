6b677825b86d9f5c1e3d33fefae6236212dcfd88
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-29 13:06:22 +0200

    feat(GreenFoxOrganisation): create field, methods and constructors in Cohort

--- week-04/day-02/src/GreenFoxOrganisation/Cohort.java
+++ week-04/day-02/src/GreenFoxOrganisation/Cohort.java
@@ -1,4 +1,30 @@
 package GreenFoxOrganisation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cohort {
+
+  String name;
+  List<Student> students;
+  List<Mentor> mentors;
+
+  public Cohort(String name) {
+    this.name = name;
+    List<Student> students = new ArrayList<>();
+    List<Mentor> mentors = new ArrayList<>();
+  }
+
+  public void addStudent(Student student) {
+    students.add(student);
+  }
+
+  public void addMentor(Mentor mentor) {
+    mentors.add(mentor);
+  }
+
+  public void info() {
+    System.out.println("The " + name + "cohort has " + students.size() +
+                       " students and " + mentors.size() + " mentors.");
+  }
 }
