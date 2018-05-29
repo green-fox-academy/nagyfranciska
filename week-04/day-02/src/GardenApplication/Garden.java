32f1bd1f1de7ebae2addebcecced0d5660123c89
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-29 15:51:22 +0200

    feat(Garden): create info method to show the state of Garden

--- week-04/day-02/src/GardenApplication/Garden.java
+++ week-04/day-02/src/GardenApplication/Garden.java
@@ -1,4 +1,42 @@
 package GardenApplication;
+import java.util.ArrayList;
 
 public class Garden {
+
+  double waterAmount;
+  ArrayList<Flower> flowers;
+  ArrayList<Tree> trees;
+
+  public Garden() {
+    flowers = new ArrayList<>();
+    trees = new ArrayList<>();
+    waterAmount = 0;
+  }
+
+  public ArrayList addFlowers(Flower flower) {
+    flowers.add(flower);
+    return flowers;
+  }
+
+  public ArrayList addTrees(Tree tree) {
+    trees.add(tree);
+    return trees;
+  }
+
+  public void info() {                    // this after watering
+    for (Flower flower : flowers) {
+      if (flower.flowerWaterAmount < 5) {
+        System.out.println("The " + flower + "Flower needs water.");
+      } else {
+        System.out.println("The " + flower + "Flower doesn't need water.");
+      }
+    }
+    for (Tree tree : trees) {
+      if (tree.treeWaterAmount < 10) {
+        System.out.println("The " + tree + "Tree needs water.");
+      } else {
+        System.out.println("The " + tree + "Tree doesn't need water.");
+      }
+    }
+  }
 }
