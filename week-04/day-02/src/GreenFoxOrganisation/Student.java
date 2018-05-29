79f8bbdaa1a416f545ef0ce2c5a167235f84acd5
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-29 12:25:07 +0200

    feat(GreenFoxOrganisation): create field, methods and constructors in Student

--- week-04/day-02/src/GreenFoxOrganisation/Student.java
+++ week-04/day-02/src/GreenFoxOrganisation/Student.java
@@ -1,4 +1,35 @@
 package GreenFoxOrganisation;
 
-public class Student {
+public class Student extends Person {
+
+  String previousOrganisation;
+  int skippedDays;
+
+  public Student (String name, int age, String gender) {
+    super(name, age, gender);
+    skippedDays = 0;
+  }
+
+  public Student (String name, int age, String gender, String previousOrganisation) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    previousOrganisation = "The School of Life";
+    skippedDays = 0;
+  }
+
+  public void getGoal() {
+    System.out.println("Be a junior software developer.");
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender +
+                       " from " + previousOrganisation + " skipped " + skippedDays +
+                       " days from the course already.");
+  }
+
+  public int skipDays(int numberOfDays) {
+    skippedDays += numberOfDays;
+    return skippedDays;
+  }
 }
