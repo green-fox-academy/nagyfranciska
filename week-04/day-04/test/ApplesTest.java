6ec97f01bdd0c7a735fbd3da848ae79c4230b77e
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-31 13:03:27 +0200

    feat(ApplesTest): create funcioning test method

--- week-04/day-04/test/ApplesTest.java
+++ week-04/day-04/test/ApplesTest.java
@@ -1,5 +1,25 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+import sun.plugin2.message.GetAppletMessage;
+
 import static org.junit.Assert.*;
 
 public class ApplesTest {
+
+  static Apples apples;
+
+  @BeforeClass
+  public static void init() {
+    apples = new Apples();
+  }
+
+  @Test
+  public void should_returnApple() {
+
+    String expected = "apple";
+    String result = apples.getApple();
+    assertEquals(expected, result);
+  }
+
 
 }
