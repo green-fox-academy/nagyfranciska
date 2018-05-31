bef35203587fd1ba528e7fed6e8e308782c9ad8b
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-31 11:38:32 +0200

    init(Apples): initiate Apples and Test classes

--- week-04/day-03/src/reserved/Reservation.java
+++ week-04/day-03/src/reserved/Reservation.java
@@ -19,24 +19,12 @@ public class Reservation implements Reservatiory {
   public String getCodeBooking() {
     String uniCode = "";
     Random b = new Random();
-
-
     for (int i = 0; i < 8; i++) {
       if (Math.random() <= 0.5) {
         uniCode += (char)(b.nextInt(26) + 'A');
       } else {
         uniCode += (char)(b.nextInt(10) + '0');
       }
-
-
-
-
-      // char c = (char) ((int) Math.random() * 10 + 89);
-//      if (c > 57 && c < 89) {
-//        uniCode += c + 8;
-//      } else if (c < 57 && c > 48) {
-//        uniCode += c;
-//      }
     }
     return uniCode;
   }
