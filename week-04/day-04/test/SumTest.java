0992e51ce5f1a137239e4b7d9155d25f20577d85
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-31 13:19:17 +0200

    feat(SumTest): add ArrayList and new Test case

--- week-04/day-04/test/SumTest.java
+++ week-04/day-04/test/SumTest.java
@@ -1,7 +1,25 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class SumTest {
 
-  
+  static Sum sum;
+  static ArrayList<Integer> listOfNumbers;
+
+  @BeforeClass
+  public static void init() {
+    sum = new Sum();
+    listOfNumbers = new ArrayList<Integer>();
+  }
+
+  @Test
+  public static int should_SumArrayList(ArrayList<Integer>) {
+    int summa = 0;
+    return summa;
+  }
 
 }
