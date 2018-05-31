7d3bfb6bb8c27c932fe098f4e9a453ca55dc87b0
nagyfranciska <nagy.franciska@yahoo.com>
2018-05-31 14:11:07 +0200

    feat(SumTest): create succeeding test in case of empty and not empty arraList

--- week-04/day-04/src/Sum.java
+++ week-04/day-04/src/Sum.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class Sum {
+
+  public static int sum(ArrayList<Integer> listOfNumbers) {
+    int sumOfNumbers = 0;
+    for (int number : listOfNumbers) {
+      sumOfNumbers += number;
+    }
+    return sumOfNumbers;
+  }
 }
--- week-04/day-04/test/SumTest.java
+++ week-04/day-04/test/SumTest.java
@@ -1,25 +1,37 @@
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import java.util.ArrayList;
-
 import static org.junit.Assert.*;
 
 public class SumTest {
 
-  static Sum sum;
-  static ArrayList<Integer> listOfNumbers;
+  static Sum sumTest;
+  static ArrayList<Integer> testList;
 
-  @BeforeClass
-  public static void init() {
-    sum = new Sum();
-    listOfNumbers = new ArrayList<Integer>();
+  @Before
+  public void init() {
+    sumTest = new Sum();
+    testList = new ArrayList<Integer>();
   }
 
   @Test
-  public static int should_SumArrayList(ArrayList<Integer>) {
-    int summa = 0;
-    return summa;
+  public void should_SumArrayList() {
+    int randomNumber1 = 2;
+    int randomNumber2 = 5;
+    int randomNumber3 = 7;
+    testList.add(randomNumber1);
+    testList.add(randomNumber2);
+    testList.add(randomNumber3);
+
+    int expected = randomNumber1 + randomNumber2 + randomNumber3;
+
+    assertEquals(expected, sumTest.sum(testList));
+  }
+
+  @Test
+  public void should_returnZero_whenEmptyArrayList() {
+    assertEquals(0, sumTest.sum(testList));
   }
 
 }
