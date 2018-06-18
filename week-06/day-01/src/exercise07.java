e06cf1bb20e7cc71d46e979340fc18ec4050b479
nagyfranciska <nagy.franciska@yahoo.com>
2018-06-18 14:18:30 +0200

    feat(Exercise07): get character frequency from String with stream, 3 solutions

--- week-06/day-01/src/exercise07.java
+++ week-06/day-01/src/exercise07.java
@@ -1,6 +1,7 @@
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
 
 public class exercise07 {
 
@@ -10,23 +11,28 @@ public class exercise07 {
 
     String example = "TTThree";
 
+
     Map<Character, Integer> frequency = example.chars()
       .mapToObj(s -> (char)s)
-      .collect(Collectors.toConcurrentMap(c -> c, c -> 1, Integer::sum));
+      .collect(Collectors.toConcurrentMap(key -> key, val -> 1, Integer::sum));
 
     System.out.println(frequency);
 
     //  or
 
-    Map<Character, Long> frequency2 = example.chars()
-      .mapToObj(s -> (char)s)
-      .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+    Map<String, Integer> frequency2 = example.chars()
+      .boxed()
+      .collect(toMap(key -> String.valueOf((char)key.intValue()), val -> 1, Integer::sum));
 
-    frequency2.forEach((ch, val) -> System.out.println(ch + " : " + val));
+    frequency2.forEach(((key, val) -> System.out.print(key + ":" + val + "\n")));
 
     //  or
 
+    Map<Character, Long> frequency3 = example.chars()
+      .mapToObj(s -> (char)s)
+      .collect(Collectors.groupingBy(key -> key, Collectors.counting()));
 
+    frequency3.forEach((key, val) -> System.out.println(key + " : " + val));
 
   }
 }
