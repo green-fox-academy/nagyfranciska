bf539edd5870160a7134c3ff793ec11e2834da05
nagyfranciska <nagy.franciska@yahoo.com>
2018-06-18 14:34:55 +0200

    feat(Exercise08): get frequency of numbers in List with stream

--- week-06/day-01/src/exercise08.java
+++ week-06/day-01/src/exercise08.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class exercise08 {
 
 
 //  Write a Stream Expression to find the frequency of numbers in the following array:
 //  ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
 
+  public static void main(String[] args) {
 
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
+
+    Map<Integer, Integer> frequency = numbers.stream()
+      .collect(Collectors.toConcurrentMap(key -> key, val -> 1, Integer::sum));
+
+    frequency.forEach((key, val) -> System.out.println(key + ":" + val));
+
+  }
 }
