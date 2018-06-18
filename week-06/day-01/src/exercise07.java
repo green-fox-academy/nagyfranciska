7b5e92863571de968ba4097b85b932ed5458a559
nagyfranciska <nagy.franciska@yahoo.com>
2018-06-18 13:44:18 +0200

    feat(Exercise07): get character frequency from String with stream

--- week-06/day-01/src/exercise07.java
+++ week-06/day-01/src/exercise07.java
@@ -1,6 +1,32 @@
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class exercise07 {
 
 //  Write a Stream Expression to find the frequency of characters in a given string!
 
+  public static void main(String[] args) {
 
+    String example = "TTThree";
+
+    Map<Character, Integer> frequency = example.chars()
+      .mapToObj(s -> (char)s)
+      .collect(Collectors.toConcurrentMap(c -> c, c -> 1, Integer::sum));
+
+    System.out.println(frequency);
+
+    //  or
+
+    Map<Character, Long> frequency2 = example.chars()
+      .mapToObj(s -> (char)s)
+      .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+
+    frequency2.forEach((ch, val) -> System.out.println(ch + " : " + val));
+
+    //  or
+
+
+
+  }
 }
