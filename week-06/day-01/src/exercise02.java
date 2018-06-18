1cca3a000ec49e6f3ab5f291f7a7184a002e0b3a
nagyfranciska <nagy.franciska@yahoo.com>
2018-06-18 11:46:50 +0200

    feat(Exercise02): get average of odd numbers with stream

--- week-06/day-01/src/exercise02.java
+++ week-06/day-01/src/exercise02.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class exercise02 {
 
 //  Write a Stream Expression to get the average value of the odd numbers from the following array:
 //  ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
 
+  public static void main(String[] args) {
 
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+    numbers.stream()
+      .filter(n -> n % 2 == 1)
+      .mapToInt(n -> n)
+      .average();
+
+  }
 }
