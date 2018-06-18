32ba95f92a2ab60e7474099660b4e60410ced58d
nagyfranciska <nagy.franciska@yahoo.com>
2018-06-18 15:16:43 +0200

    feat(Exercise09): convert char array to String with stream via function

--- week-06/day-01/src/exercise09.java
+++ week-06/day-01/src/exercise09.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class exercise09 {
 
 //  Write a Stream Expression to convert a char array to a string!
 
+  public static void main(String[] args) {
+
+    Character[] characterArray = new Character[] {'W', 'e', 'l', 'c', 'o', 'm' , 'e', ' ', 't', 'o', ' ', 't', 'h', 'e', ' ', 'J', 'u', 'n', 'g', 'l', 'e'};
+    System.out.println(convertCharArrayToString(characterArray));
+
+  }
+
+  private static String convertCharArrayToString(Character[] example) {
+    return Arrays.stream(example)
+      .map(c -> Character.toString(c))
+      .reduce((word, character) -> word + character)
+      .get();
+
+  }
 }
